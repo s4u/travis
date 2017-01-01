6f72fcb00ba550d5203a2d819b9ecbeb66c5b2ca
Slawomir Jaranowski <s.jaranowski@gmail.com>
2017-01-01 16:40:59 +0100

    skip build for maven prepare release

--- src/skip.sh
+++ src/skip.sh
@@ -4,3 +4,8 @@ if [ "$TRAVIS_TAG" ]; then
     echo "$0 skip build for tag: $TRAVIS_TAG"
     exit 0
 fi
+
+if git log -1 --format=%B $TRAVIS_COMMIT | grep -q "\[maven-release-plugin\] prepare release"; then
+    echo "$0 skip build for maven prepare release"
+    exit 0
+fi
