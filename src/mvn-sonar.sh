d09c4433475dcd85ec5e4ccf362bc37962687066
Slawomir Jaranowski <s.jaranowski@gmail.com>
2019-11-04 23:13:58 +0100

    sonar config

--- src/mvn-sonar.sh
+++ src/mvn-sonar.sh
@@ -2,6 +2,27 @@
 
 _NEED_TAG=1
 
-echo "$SONARQUBE_SCANNER_PARAMS"
+if [ -z "$SONARQUBE_SCANNER_PARAMS" \
+        -a ! "$TRAVIS_PULL_REQUEST" = "false" \
+        -a -n "$TRAVIS_PULL_REQUEST_SLUG" \
+        -a -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+    echo "Setup sonar for PR"
+    if [ -n "$SONAR_TOKEN" ]; then
+        _ST=$SONAR_TOKEN
+    else
+        _ST=`echo "OTg3ZmJmNjk5NGVmNDljZDU4MTcyYWU5N2E4MjgyYTZkN2VmODZjYwo=" | openssl base64 -d`
+    fi
+    export SONARQUBE_SCANNER_PARAMS="{\
+        \"sonar.pullrequest.key\" : \"$TRAVIS_PULL_REQUEST\",\
+        \"sonar.pullrequest.base\": \"$TRAVIS_BRANCH\",\
+        \"sonar.pullrequest.branch\": \"$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH\",\
+        \"sonar.pullrequest.github.repository\": \"$TRAVIS_REPO_SLUG\",\
+        \"sonar.login\": \"$_ST\"}"
+fi
+
+if [ -z "$SONARQUBE_SCANNER_PARAMS" ]; then
+    echo "no configuration for sonar - skip"
+    exit 0
+fi
 
 . `dirname $0`/mvn-run.sh $@
